52963f3a3e5db1ab8b4555e9258410b87b4f3ee1
jake-adderley <111409431+jake-adderley@users.noreply.github.com>
2022-10-12 13:29:27 +1100

    minor changes to graphs

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -6,3 +6,7 @@ var vg_2 = "js/main_map_test.vg.json";
 vegaEmbed("#main_map_test", vg_2).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+var vg_3 = "js/storm_count_chart.vg.json";
+vegaEmbed("#storm_count_graph", vg_3).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
